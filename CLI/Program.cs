dd4b6d115d5aeca19fdf57444692466632c11157
Stuart Clark <stuart.clark@education.govt.nz>
2020-11-03 15:29:05 +1300

    Add Devops Files

---
+++ CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoE.ECE.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
