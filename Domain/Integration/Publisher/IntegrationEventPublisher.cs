1be34bf47d987718ea070b58bc8bb33ee2ce6110
Stuart Clark <stuart.clark@education.govt.nz>
2020-12-04 09:01:22 +1300

    wip

--- Domain/Integration/Publisher/IntegrationEventPublisher.cs
+++ Domain/Integration/Publisher/IntegrationEventPublisher.cs
@@ -1,10 +1,17 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using Events.Integration.Protobuf.Workflow;
+using Google.Type;
+using Marten;
 using MoE.ECE.Domain.Event;
 using MoE.ECE.Domain.Event.OperationalFunding;
+using MoE.ECE.Domain.Infrastructure.EntityFramework;
+using MoE.ECE.Domain.Model.OperationalFunding;
+using MoE.ECE.Domain.Read.Model.Rs7;
 using Moe.ECE.Events.Integration;
 using Moe.Library.Cqrs;
+using ProtoBuf.Bcl;
 using IntegrationEvents = Events.Integration.Protobuf;
 namespace MoE.ECE.Domain.Integration.Publisher
 {
@@ -21,11 +28,15 @@ namespace MoE.ECE.Domain.Integration.Publisher
     {
         private readonly IServiceBus _serviceBus;
         private readonly IMapper _mapper;
+        private readonly IDocumentSession _documentSession;
+        private readonly ReferenceDataContext _referenceDataContext;
 
-        public IntegrationEventPublisher(IServiceBus serviceBus, IMapper mapper)
+        public IntegrationEventPublisher(IServiceBus serviceBus, IMapper mapper, IDocumentSession documentSession, ReferenceDataContext referenceDataContext)
         {
             _serviceBus = serviceBus;
             _mapper = mapper;
+            _documentSession = documentSession;
+            _referenceDataContext = referenceDataContext;
         }
 
         public Task Handle(Rs7EntitlementMonthUpdated domainEvent, CancellationToken cancellationToken)
@@ -46,7 +57,11 @@ namespace MoE.ECE.Domain.Integration.Publisher
         {
             var integrationEvent = _mapper.Map<IntegrationEvents.Roll.Rs7Updated>(domainEvent);
 
-            return _serviceBus.PublishAsync(integrationEvent, Constants.Topic.ECE, cancellationToken);
+            var rs7UpdatedTask = _serviceBus.PublishAsync(integrationEvent, Constants.Topic.ECE, cancellationToken);
+
+            var fundingEntitlementUpdatedTask = PublishFundingEntitlementUpdated(cancellationToken, domainEvent);
+
+            return Task.WhenAll(rs7UpdatedTask, fundingEntitlementUpdatedTask);
         }
 
         public Task Handle(Rs7PeerRejected domainEvent, CancellationToken cancellationToken)
@@ -90,6 +105,52 @@ namespace MoE.ECE.Domain.Integration.Publisher
 
             return _serviceBus.PublishAsync(
                 integrationEvent,
+                Constants.Topic.ECE,
+                cancellationToken);
+        }
+        
+        private async Task PublishFundingEntitlementUpdated(CancellationToken cancellationToken,
+            Rs7Model domainEvent)
+        {
+            var operationalFundingRequest = await _documentSession
+                    .Query<OperationalFundingRequest>()
+                    .SingleOrDefaultAsync(model => model.BusinessEntityId == domainEvent.BusinessEntityId 
+                                              && model.RevisionNumber == domainEvent.RevisionNumber, cancellationToken);
+            
+            var service = await _referenceDataContext.EceServices.FindAsync(domainEvent.OrganisationId);
+            
+            var organisationNumber = service?.OrganisationNumber ?? string.Empty;
+            
+            var fundingEntitlementUpdated = new FundingEntitlementUpdated
+            {
+                Id = new Guid(domainEvent.BusinessEntityId.GetValueOrDefault()),
+                Description =
+                    $"{Constants.BusinessEntityTypes.Rs7} request: {domainEvent.RequestId}",
+                EntitlementType = Constants.BusinessEntityTypes.Rs7,
+                // NEED TO GET FROM OPA NEED TO GET FROM OPA
+                PaymentNzDate = new Date
+                {
+                    Year = System.DateTime.UtcNow.AddDays(1).Year, Month = System.DateTime.UtcNow.AddDays(1).Month, Day = System.DateTime.UtcNow.AddDays(1).Day
+                },
+                PayeeOrganisationNumber = organisationNumber,
+                ParentPayeeOrganisationNumber = organisationNumber,
+                SourceSystem = Constants.Topic.ECE,
+                BusinessEntityId = new Guid(domainEvent.BusinessEntityId.GetValueOrDefault()),
+                LineItems =
+                {
+                    new InvoiceLineItem
+                    {
+                        AmountGstExclusive = new Decimal(operationalFundingRequest.TotalWashUp.GetValueOrDefault()),
+                        AmountGstInclusive = new Decimal(operationalFundingRequest.TotalWashUp.GetValueOrDefault()),
+                        Description = $"Line item 1"
+                    }
+                },
+
+                Status = FundingEntitlementUpdatedStatus.EntitlementApproved
+            };
+
+            await _serviceBus.PublishAsync(
+                fundingEntitlementUpdated,
                 Constants.Topic.ECE,
                 cancellationToken);
         }
